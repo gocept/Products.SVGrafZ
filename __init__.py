b77038e9ac5505bb0a7bcf3f54682663d9886c53
Michael Howitz <mh@gocept.com>
2003-04-14 14:14:02 +0000

    SVGrafZ is Now addable via ZMI

--- __init__.py
+++ __init__.py
@@ -1,13 +1,24 @@
 ################################################################################
 ## 
 ## SVGrafZ
-## Version: $Id: __init__.py,v 1.1 2003/04/09 12:25:55 mac Exp $
+## Version: $Id: __init__.py,v 1.2 2003/04/14 14:14:02 mac Exp $
 ##
 ################################################################################
 
 from registry import Registry
 from types import BarGraphs
 from bar import SimpleBarGraph
+from svgrafz import SVGrafZProduct
 
-Registry.registerKind(BarGraphs, SimpleBarGraph)
 
+from svgrafz import SVGrafZProduct, addForm, addFunction
+
+def initialize(registrar):
+    # register diagramkinds
+    Registry.registerKind(BarGraphs, SimpleBarGraph)
+    
+    registrar.registerClass(
+        SVGrafZProduct, 
+        constructors = (addForm, addFunction),
+        icon = 'www/icon.gif'
+        )
--- svgrafz.py
+++ svgrafz.py
@@ -1,13 +1,15 @@
 ################################################################################
 ## 
 ## SVGrafZ
-## Version: $Id: svgrafz.py,v 1.2 2003/04/11 14:06:54 mac Exp $
+## Version: $Id: svgrafz.py,v 1.3 2003/04/14 14:14:02 mac Exp $
 ##
 ################################################################################
 
 
 from OFS.SimpleItem import SimpleItem
 from registry import Registry
+from AccessControl import ClassSecurityInfo
+from Globals import InitializeClass
 
 def getDiagramKinds():
     """Get the names of the DiagramKinds."""
@@ -24,7 +26,44 @@ class SVGrafZProduct(SimpleItem):
                  
                  )
 
-    
-    def __init__(self, id, title):
+    security=ClassSecurityInfo()
+
+
+    def __init__(self, id, title, graph):
         self.id    = id
         self.title = title
+        self.namegraph = graph
+
+
+    security.declarePublic('html')
+    def html(self):
+        print '<object type="image/svg+xml" width="600" height="300" data="self">Ihr Browser unterstützt keine SVG-Grafiken. Schade.</object>'
+
+        
+
+InitializeClass(SVGrafZProduct)
+
+
+def addForm(dispatcher):
+    """Returns an HTML form."""
+    
+    return """<html>
+    <head><title>Add SVGrafZ</title></head>
+    <body>   
+    <form action="addFunction">
+    id: <input type="text" name="id"/><br/>
+    title: <input type="text" name="title"/><br/>
+    diagramKind:<select height="1" name="graph">
+    <option>SimpleBarGraph</option>
+    </select>
+    <br/>
+    <input type="submit"/>
+    </form>
+    </body>
+    </html>"""
+
+
+def addFunction(dispatcher, id, title, graph):
+    """Create a new graph and add it to myself."""
+    g = SVGrafZProduct(id, title, graph)
+    dispatcher.Destination()._setObject(id, g)
